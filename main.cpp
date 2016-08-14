891169612bbfa75e5287bd95cdd2673b581b3650
Julian Pitney <julianpitney@Gmail.com>
2016-08-14 12:22:14 -0400

    I forget what I did but i'm commiting it

--- main.cpp
+++ main.cpp
@@ -1,51 +1,13 @@
 #include <console_calender.h>
 #include <console_planner.h>
-
-
-// Scans LLCalender to determine whether the current day is later than the day the last node was added to the list.
-// If it is, it adds a node for today to the list and returns true, otherwise it does nothing and retrns false.
-bool startup_proc(LLCalender* input_calender)
-{
-	if (input_calender == NULL) // Case: Received NULL ptr argument
-	{
-		cout << "Error: startup_proc() received NULL argument\n";
-		return false;
-	}
-	else if (input_calender->last_node == NULL) // Case: input_calender LL is empty
-	{
-		input_calender->appendToday();
-		return true;
-	}
-
-	struct tm *now = getCurrentTime();
-	if (input_calender->last_node->dayInitTime->tm_year < now->tm_year) // Case: Last node was added to input_calender at an earlier year
-	{
-		delete now;
-		input_calender->appendToday();
-		return true;
-	}
-	else if (input_calender->last_node->dayInitTime->tm_yday < now->tm_yday) // Case: Last node was added to input_calender at an earlier day
-	{
-		delete now;
-		input_calender->appendToday();
-		return true;
-	}
-	else // Case: Last node was added to input_calender today
-	{
-		delete now;
-		return false;
-	}
-
-
-	
-}
-
+#include <startup.h>
 
 
 int main(int argc, char *argv[])
 {
 	LLCalender *listHandle = new LLCalender();
 	startup_proc(listHandle);
+	cout << listHandle->getDayNode(0) << endl;
 	
-	
+
 }
---
+++ startup.h
@@ -0,0 +1,4 @@
+#include<console_planner.h>
+
+
+bool startup_proc(LLCalender* input_calender);
